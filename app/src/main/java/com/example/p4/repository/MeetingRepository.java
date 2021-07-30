bb7130b28323e527e43263043d98e0fe0187e3ec
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-30 18:43:42 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/repository/MeetingRepository.java
+++ app/src/main/java/com/example/p4/repository/MeetingRepository.java
@@ -1,8 +1,6 @@
 package com.guilhempelissier.mareu.repository;
 
 
-//Dans une vraie app, cette classe fait l'arbitrage entre les données persistées en local et les
-//call API quand les données locales ne sont pas fraiches.
 
 public class MeetingRepository {
 
