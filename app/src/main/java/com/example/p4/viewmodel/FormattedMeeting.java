dfd6af4ae6368c609cc2c82cc7d126351391c837
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-30 18:45:47 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/viewmodel/FormattedMeeting.java
+++ app/src/main/java/com/example/p4/viewmodel/FormattedMeeting.java
@@ -1,37 +1,5 @@
-package com.guilhempelissier.mareu.viewmodel;
+package com.example.p4.viewmodel;
 
 public class FormattedMeeting {
-	private String title;
-	private String description;
-	private String id;
 
-	public FormattedMeeting(String title, String description, String id) {
-		this.title = title;
-		this.description = description;
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
 }
