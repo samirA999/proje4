8fded885435a8e769cda0ce58a328769f3ce2553
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-30 18:42:38 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/model/Room.java
+++ app/src/main/java/com/example/p4/model/Room.java
@@ -1,34 +1,6 @@
-package com.guilhempelissier.mareu.model;
+package com.example.p4.model;
 
-import java.util.UUID;
 
 public class Room {
-	private String id;
-	private String name;
 
-	public Room(String name, String id) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public Room(String name) {
-		this.id = UUID.randomUUID().toString();
-		this.name = name;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
