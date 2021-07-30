6b819ba9ab4d38c3f76c57b0fc762be4f1e9d335
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-07-30 18:47:11 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/view/ui/MainActivity.java
+++ app/src/main/java/com/example/p4/view/ui/MainActivity.java
@@ -1,34 +1,37 @@
-package com.example.p4;
+package com.example.p4.view.ui;
 
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.p4.view.ui.MeetingListViewModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.example.p4.viewmodel.MeetingListViewModel;
+import com.example.p4.R;
 import com.example.p4.view.adapter.MeetingListAdapter;
-import com.example.p4.viewmodel.FormttedMeeting;
-public class MainActivity<MeetingListViewModel> extends AppCompatActivity {
+import com.example.p4.viewmodel.FormattedMeeting;
+import com.example.p4.viewmodel.MeetingListViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NewMeetingDialog.NewMeetingDialogListener, FilterDialog.FilterDialogListener {
     private RecyclerView recyclerView;
     private MeetingListViewModel meetingListViewModel;
     private MeetingListAdapter meetingListAdapter;
 
     private FloatingActionButton newMeetingButton;
 
-    private LiveData<List<FormttedMeeting>> meeting;
-
+    private LiveData<List<FormattedMeeting>> meetings;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState ){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.main_toolbar);
@@ -36,24 +39,58 @@ public class MainActivity<MeetingListViewModel> extends AppCompatActivity {
 
         newMeetingButton = findViewById(R.id.meeting_list_addMettingBtn);
 
-        newMeetingButton.setOnClickListener(view -> showNewMeeetingDialog());
+        newMeetingButton.setOnClickListener(view -> showNewMeetingDialog());
 
-        meetingListViewModel = ViewModelProvider.of(this).get(com.example.p4.view.ui.MeetingListViewModel.class);
-        meeting.observe(this, formttedMeetingList -> meetingListAdapter.setData(formttedMeetingList) );
+        meetingListViewModel = ViewModelProviders.of(this).get(MeetingListViewModel.class);
+        meetings = meetingListViewModel.getMeetingListObservable();
+
+        recyclerView = findViewById(R.id.meeting_list_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        meetingListAdapter = new MeetingListAdapter(meetings.getValue());
+        recyclerView.setAdapter(meetingListAdapter);
+
+        meetings.observe(this, formattedMeetings -> meetingListAdapter.setData (formattedMeetings));
 
         meetingListAdapter.setOnItemDeleteListener(id -> meetingListViewModel.deleteMeetingById(id));
-
-
     }
 
-    public void showNewMeeetingDialog()
+    public void showNewMeetingDialog() {
         NewMeetingDialog dialog = new NewMeetingDialog();
-        dialog.show (getSupportFragmentManager(), "NewMeetingDialog");
+        dialog.show(getSupportFragmentManager(), "NewMeetingDialog");
     }
 
-    public void showFilterDialog(){
+    public void showFilterDialog() {
         MeetingListFilter currentFilter = meetingListViewModel.getMeetingListFilter().getValue();
-        if (currentFilter != null){
-            FilterDialog dialog = new FilterDialog(currentFilter)
+        if (currentFilter != null) {
+            FilterDialog dialog = new FilterDialog(currentFilter);
+            dialog.show(getSupportFragmentManager(), "FilterDialog");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.filter_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_filter) {
+            showFilterDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDialogPositiveClick(String topic, String place, long time, List<String> participants) {
+        meetingListViewModel.addNewMeeting(topic, place, time, participants);
+    }
+
+    @Override
+    public void onDialogPositiveClick(long startDateFilter, long stopDateFilter, List<String> allowedPlacesFilter) {
+        meetingListViewModel.setStartHourFilter(startDateFilter);
+        meetingListViewModel.setEndHourFilter(stopDateFilter);
+        meetingListViewModel.setAllowedPlaces(allowedPlacesFilter);
+    }
 }
--- app/src/main/java/com/example/p4/view/ui/NewMeetingDialog.java
+++ app/src/main/java/com/example/p4/view/ui/NewMeetingDialog.java
@@ -1,4 +1,4 @@
-package com.guilhempelissier.mareu.view.ui;
+package com.example.p4.view.ui;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
@@ -14,7 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import com.guilhempelissier.mareu.R;
+import com.example.p4.R;
 import com.hootsuite.nachos.NachoTextView;
 import com.hootsuite.nachos.terminator.ChipTerminatorHandler;
 import com.hootsuite.nachos.validator.ChipifyingNachoValidator;
@@ -25,83 +25,82 @@ import java.util.Date;
 import java.util.List;
 
 public class NewMeetingDialog extends DialogFragment {
-	private NewMeetingDialogListener listener;
 
-	private EditText topicEditText;
-	private EditText placeEditText;
-	private Button datePickerButton;
-	private NachoTextView chipsTextView;
+    private NewMeetingDialogListener listener;
+    private EditText topicEditText;
+    private EditText placeEditText;
+    private Button datePickerButton;
+    private NachoTextView chipsTextView;
+    private DateFormat df = DateFormat.getDateTimeInstance();
+    private Calendar calendar = Calendar.getInstance();
 
-	private DateFormat df = DateFormat.getDateTimeInstance();
-	private Calendar calendar = Calendar.getInstance();
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener =(NewMeetingDialogListener) context;
 
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		listener = (NewMeetingDialogListener) context;
-	}
+    }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final View view = requireActivity().getLayoutInflater().inflate(R.layout.content_main, null) ;
 
-	@NonNull
-	@Override
-	public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		final View view = requireActivity().getLayoutInflater().inflate(R.layout.dialog_add_new_meeting, null);
+        calendar.setTime(new Date());
 
-		calendar.setTime(new Date());
+        topicEditText = view.findViewById(R.id.newMeetingTopicEditText);
+        placeEditText = view.findViewById(R.id.newMeetingPlaceEditText);
+        datePickerButton = view.findViewById(R.id.newMeetingDatePickerButton);
+        datePickerButton.setText(df.format(new Date(calendar.getTimeInMillis())));
+        chipsTextView = view.findViewById(R.id.newMeetingChipsTextView);
 
-		topicEditText = view.findViewById(R.id.newMeetingTopicEditText);
-		placeEditText = view.findViewById(R.id.newMeetingPlaceEditText);
-		datePickerButton = view.findViewById(R.id.newMeetingDatePickerButton);
-		datePickerButton.setText(df.format(new Date(calendar.getTimeInMillis())));
-		chipsTextView = view.findViewById(R.id.newMeetingChipsTextView);
+        chipsTextView.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
+        chipsTextView.addChipTerminator( ' ', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
+        chipsTextView.enableEditChipOnTouch(true, true);
+        chipsTextView.setNachoValidator(new ChipifyingNachoValidator());
 
-		chipsTextView.addChipTerminator('\n', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
-		chipsTextView.addChipTerminator( ' ', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
-		chipsTextView.enableEditChipOnTouch(true, true);
-		chipsTextView.setNachoValidator(new ChipifyingNachoValidator());
+        setButtonOnClick();
 
-		setButtonOnClick();
+        builder.setTitle(R.string.add_new_meeting)
+                .setPositiveButton(R.string.new_meeting_positive, (dialogInterface, i) -> listener.onDialogPositiveClick(
+                        topicEditText.getText().toString(),
+                        placeEditText.getText().toString(),
+                        calendar.getTimeInMillis(),
+                        chipsTextView.getChipValues()))
+                .setNeutralButton(R.string.new_metting_negative, (dialogInterface, i) -> NewMeetingDialog.this.getDialog().cancel())
+                .setView(view);
 
-		builder.setTitle(R.string.add_new_meeting)
-				.setPositiveButton(R.string.new_meeting_positive, (dialogInterface, i) -> listener.onDialogPositiveClick(
-						topicEditText.getText().toString(),
-						placeEditText.getText().toString(),
-						calendar.getTimeInMillis(),
-						chipsTextView.getChipValues()))
-				.setNeutralButton(R.string.new_metting_negative, (dialogInterface, i) -> NewMeetingDialog.this.getDialog().cancel())
-				.setView(view);
+        return builder.create();
+    }
 
-		return builder.create();
-	}
+    private void setButtonOnClick() {
+        datePickerButton.setOnClickListener(view1 -> {
+            DatePickerDialog dateDialog = new DatePickerDialog(requireContext(),
+                    (datePicker, year, month, dayOfMonth) -> {
+                        calendar.set(year, month, dayOfMonth);
+                        TimePickerDialog timeDialog = new TimePickerDialog(requireContext(),
+                                (timePicker, hourOfDay, minute) -> {
+                                    calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                    calendar.set(Calendar.MINUTE, minute);
+                                    datePickerButton.setText(
+                                            df.format(new Date(calendar.getTimeInMillis()))
+                                    );
+                                },
+                                calendar.get(Calendar.HOUR_OF_DAY),
+                                calendar.get(Calendar.MINUTE),
+                                true
+                        );
+                        timeDialog.show();
+                    },
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            dateDialog.show();
+        });
+    }
 
-	private void setButtonOnClick() {
-		datePickerButton.setOnClickListener(view1 -> {
-			DatePickerDialog dateDialog = new DatePickerDialog(requireContext(),
-					(datePicker, year, month, dayOfMonth) -> {
-						calendar.set(year, month, dayOfMonth);
-						TimePickerDialog  timeDialog = new TimePickerDialog(requireContext(),
-								(timePicker, hourOfDay, minute) -> {
-									calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-									calendar.set(Calendar.MINUTE, minute);
-									datePickerButton.setText(
-											df.format(new Date(calendar.getTimeInMillis()))
-									);
-								},
-								calendar.get(Calendar.HOUR_OF_DAY),
-								calendar.get(Calendar.MINUTE),
-								true
-						);
-						timeDialog.show();
-					},
-					calendar.get(Calendar.YEAR),
-					calendar.get(Calendar.MONTH),
-					calendar.get(Calendar.DAY_OF_MONTH)
-			);
-			dateDialog.show();
-		});
-	}
-
-	public interface NewMeetingDialogListener {
-		void onDialogPositiveClick(String topic, String place, long time, List<String> participants);
-	}
+    public interface NewMeetingDialogListener {
+        void onDialogPositiveClick(String topic, String place, long time, List<String> participants);
+    }
 }
