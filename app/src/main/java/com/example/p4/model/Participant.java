3a77961ae48d15ca9d752d62e8728713541fb064
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-08-03 14:19:59 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/model/Participant.java
+++ app/src/main/java/com/example/p4/model/Participant.java
@@ -1,9 +1,35 @@
-package com.guilhempelissier.mareu.model;
+package com.example.p4.model;
 
 
+import java.util.UUID;
 
 public class Participant {
+	private String id;
+	private String emailAddress;
 
-    public Participant(String s) {
-    }
+	public Participant(String emailAddress, String id) {
+		this.id = id;
+		this.emailAddress = emailAddress;
+	}
+
+	public Participant(String emailAddress) {
+		this.id = UUID.randomUUID().toString();
+		this.emailAddress = emailAddress;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
 }
--- app/src/main/java/com/example/p4/model/Room.java
+++ app/src/main/java/com/example/p4/model/Room.java
@@ -1,6 +1,35 @@
 package com.example.p4.model;
 
 
-public class Room {
+import java.util.UUID;
 
+public class Room {
+	private String id;
+	private String name;
+
+	public Room(String name, String id) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Room(String name) {
+		this.id = UUID.randomUUID().toString();
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
