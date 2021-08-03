bcba3ff4536c17e35725fa1235b2246511d69ee2
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-08-03 14:22:04 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/view/adapter/MeetingListAdapter.java
+++ app/src/main/java/com/example/p4/view/adapter/MeetingListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.p4.view.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
