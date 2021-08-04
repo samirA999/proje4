a53470f0801f563515771b7c7115aba0e4b2be17
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-08-04 21:26:25 +0200

    Add files via upload

--- app/src/main/java/com/example/p4/view/adapter/MeetingListAdapter.java
+++ app/src/main/java/com/example/p4/view/adapter/MeetingListAdapter.java
@@ -1,46 +1,92 @@
 package com.example.p4.view.adapter;
 
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import com.example.p4.databinding.FragmentMeetingBinding;
+import com.example.p4.viewmodel.FormattedMeeting;
 
-import come.example.p4.viewmodel.FormattedMeeting;
+import java.util.List;
 
 public class MeetingListAdapter extends RecyclerView.Adapter<MeetingListAdapter.MeetingViewHolder> {
 
+    private List<FormattedMeeting> mMeetings;
+    private OnDeleteClickListener listener;
 
-    //implémenté les méthodes
+    public MeetingListAdapter(List<FormattedMeeting> meetings) {
+        mMeetings = meetings;
+    }
+
+    public void setData(List<FormattedMeeting> newData) {
+        this.mMeetings = newData;
+        notifyDataSetChanged();
+    }
     @NonNull
     @Override
-    public MeetingViewHolder onCreateViewHolder(@NonNull  ViewGroup parent , int viewType) {
+    public MeetingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         FragmentMeetingBinding binding = FragmentMeetingBinding.inflate(layoutInflater, parent, false);
-
-        return null;
+        return new MeetingViewHolder(binding);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull MeetingListAdapter.MeetingViewHolder holder , int position) {
-
+    public void onBindViewHolder(@NonNull MeetingViewHolder holder, int position) {
+        FormattedMeeting meeting = mMeetings.get(position);
+        holder.bind(meeting);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mMeetings.size();
     }
 
     public class MeetingViewHolder extends RecyclerView.ViewHolder {
+        private final FragmentMeetingBinding binding;
+
+        public MeetingViewHolder(FragmentMeetingBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+            binding.meetingDeleteBtn.setOnClickListener(view -> {
+                int position = getAdapterPosition();
+
+                if (listener != null && position != RecyclerView.NO_POSITION) {
+                    listener.onDelete(mMeetings.get(position).getId());
+                }
+            });
+
+            binding.ContraintLayout.setOnClickListener(view -> {
+                if (binding.meetingTitleTextview.getMaxLines() == 1) {
+                    binding.meetingTitleTextview.setMaxLines(15);
+                } else {
+                    binding.meetingTitleTextview.setMaxLines(1);
+                }
+
+                if (binding.meetingDescriptionTextview.getMaxLines() == 1) {
+                    binding.meetingDescriptionTextview.setMaxLines(15);
+                } else {
+                    binding.meetingDescriptionTextview.setMaxLines(1);
+                }
+            });
+        }
+
+        public void bind(FormattedMeeting meeting) {
+            binding.setDescription(meeting.getDescription());
+            binding.setTitle(meeting.getTitle());
+            binding.executePendingBindings();
+        }
     }
 
-    private List<com.guilhempelissier.mareu.viewmodel.FormattedMeeting>;
-    private OnDeleteClickListener listener;
-    public MeetingListAdapter(List<FormattedMeeyting> meetings ) {mMeetings = meetings ; }
-    public void setData
+    public interface OnDeleteClickListener {
+        void onDelete(String id);
+    }
 
+    public void setOnItemDeleteListener(OnDeleteClickListener listener) {
+        this.listener = listener;
+    }
 }
