db5206d6284d36782a722f004302eb31415d3449
samira hadjri <85124040+samirA999@users.noreply.github.com>
2021-08-17 12:08:34 +0200

    Add files via upload

--- app/src/androidTest/java/com/example/p4/utils/DeleteViewAction.java
+++ app/src/androidTest/java/com/example/p4/utils/DeleteViewAction.java
@@ -1,1 +1,29 @@
+package com.example.p4.utils;
 
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+
+import com.example.p4.R;
+
+import org.hamcrest.Matcher;
+
+public class DeleteViewAction implements ViewAction {
+	@Override
+	public Matcher<View> getConstraints() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Click on specific button";
+	}
+
+	@Override
+	public void perform(UiController uiController, View view) {
+		View button = view.findViewById(R.id.meeting_delete_btn);
+		// Maybe check for null
+		button.performClick();
+	}
+}
